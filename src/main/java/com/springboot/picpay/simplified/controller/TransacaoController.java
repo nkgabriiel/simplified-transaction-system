cb75d859ec8288881ca9e082fc59bbdb689d87eb
nkgabriiel <gabriel.3930@gmail.com>
2026-08-07 14:02:04 -0300

    fix: constraint verification bug with jakarta valid

--- src/main/java/com/springboot/picpay/simplified/controller/TransacaoController.java
+++ src/main/java/com/springboot/picpay/simplified/controller/TransacaoController.java
@@ -3,6 +3,7 @@ package com.springboot.picpay.simplified.controller;
 import com.springboot.picpay.simplified.dto.request.TransacaoRequestDTO;
 import com.springboot.picpay.simplified.dto.response.TransacaoResponseDTO;
 import com.springboot.picpay.simplified.service.TransacaoService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -19,7 +20,7 @@ public class TransacaoController {
 
     @PostMapping("/transfer")
     public ResponseEntity<TransacaoResponseDTO> criar(
-            @RequestBody TransacaoRequestDTO dto) {
+            @RequestBody @Valid TransacaoRequestDTO dto) {
         return ResponseEntity.status(HttpStatus.CREATED).body(transacaoService.gerarTransacao(dto));
     }
 
--- src/main/java/com/springboot/picpay/simplified/dto/request/TransacaoRequestDTO.java
+++ src/main/java/com/springboot/picpay/simplified/dto/request/TransacaoRequestDTO.java
@@ -1,10 +1,19 @@
 package com.springboot.picpay.simplified.dto.request;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+
 import java.math.BigDecimal;
 
 public record TransacaoRequestDTO(
+        @NotNull(message = "Remetente é obrigatório")
         Long remetenteId,
+
+        @NotNull(message = "Destinatário é obrigatório")
         Long destinatarioId,
+
+        @NotNull
+        @Positive(message = "Valor deve ser positivo")
         BigDecimal valor
 ) {
 }
