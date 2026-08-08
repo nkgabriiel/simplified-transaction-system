510121e1e33119273ae6770dcdb54a7c029ffc23
nkgabriiel <gabriel.3930@gmail.com>
2026-08-08 12:58:48 -0300

    fix: change imports to prevent future bugs

--- src/test/java/com/springboot/picpay/simplified/service/UsuarioServiceTest.java
+++ src/test/java/com/springboot/picpay/simplified/service/UsuarioServiceTest.java
@@ -21,7 +21,9 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import java.math.BigDecimal;
 import java.util.Optional;
 
-import static org.assertj.core.api.AssertionsForClassTypes.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
