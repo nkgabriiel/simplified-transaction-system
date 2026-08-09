ac8badc77575609e4c166e7e6f0c710a732c5b31
nkgabriiel <gabriel.3930@gmail.com>
2026-08-09 19:14:30 -0300

    fix: removed unused functions

--- src/main/java/com/springboot/picpay/simplified/repository/UsuarioRepository.java
+++ src/main/java/com/springboot/picpay/simplified/repository/UsuarioRepository.java
@@ -6,8 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
-    Optional<Usuario> findByDocumento (String documento);
-    Optional<Usuario> findByEmail (String email);
     boolean existsByDocumento (String documento);
     boolean existsByEmail (String email);
 }
