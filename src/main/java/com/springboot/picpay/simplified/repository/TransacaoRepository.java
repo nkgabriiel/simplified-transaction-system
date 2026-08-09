829f9840b2e1ee4e71b20906deee54bc0a677463
nkgabriiel <gabriel.3930@gmail.com>
2026-08-09 19:24:26 -0300

    fix: removed redundant function

--- src/main/java/com/springboot/picpay/simplified/repository/TransacaoRepository.java
+++ src/main/java/com/springboot/picpay/simplified/repository/TransacaoRepository.java
@@ -4,9 +4,7 @@ import com.springboot.picpay.simplified.model.Transacao;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface TransacaoRepository extends JpaRepository<Transacao, Long> {
-    Optional<Transacao> findById (Long id);
     List<Transacao> findByRemetenteIdOrDestinatarioId(Long remetenteId, Long DestinatarioId);
 }
