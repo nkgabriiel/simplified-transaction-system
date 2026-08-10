907e0dd12186362c035af5f8b4bd5b385daf722b
nkgabriiel <gabriel.3930@gmail.com>
2026-08-10 14:27:23 -0300

    feat: add swagger/openapi documentation

--- src/main/java/com/springboot/picpay/simplified/controller/TransacaoController.java
+++ src/main/java/com/springboot/picpay/simplified/controller/TransacaoController.java
@@ -3,6 +3,10 @@ package com.springboot.picpay.simplified.controller;
 import com.springboot.picpay.simplified.dto.request.TransacaoRequestDTO;
 import com.springboot.picpay.simplified.dto.response.TransacaoResponseDTO;
 import com.springboot.picpay.simplified.service.TransacaoService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -14,16 +18,32 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/transacoes")
 @RequiredArgsConstructor
+@Tag(name = "Transações", description = "Gerenciamento das transações")
 public class TransacaoController {
 
     private final TransacaoService transacaoService;
 
+    @Operation(summary = "Gerar uma nova transferência", description = "Cria uma nova transferência envolvendo dois usuários")
+    @ApiResponses({
+        @ApiResponse(responseCode = "201", description = "Transação criada com sucesso"),
+            @ApiResponse(responseCode = "400", description = "Dados inválidos"),
+            @ApiResponse(responseCode = "403", description = "Sem autorização"),
+            @ApiResponse(responseCode = "404", description = "Usuário não encontrado"),
+            @ApiResponse(responseCode = "409", description = "Chave de idempotência reutilizada com dados diferentes " +
+                    "ou concorrência na atualização do saldo"),
+            @ApiResponse(responseCode = "503", description = "Autorizador indisponível")
+    })
     @PostMapping("/transferir")
     public ResponseEntity<TransacaoResponseDTO> criar(
             @RequestBody @Valid TransacaoRequestDTO dto) {
         return ResponseEntity.status(HttpStatus.CREATED).body(transacaoService.gerarTransacao(dto));
     }
 
+    @Operation(summary = "Lista todas as transferências de um usuário", description = "Lista todas as transações em que um usuário participa" +
+            "como remetente ou destinatário")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Lista retornada com sucesso"),
+    })
     @GetMapping
     public ResponseEntity<List<TransacaoResponseDTO>> listarTodasTransacoesPorUsuario(@RequestParam Long usuarioId) {
         return ResponseEntity.ok(transacaoService.listarTodasPorUsuario(usuarioId));
--- src/main/java/com/springboot/picpay/simplified/controller/UsuarioController.java
+++ src/main/java/com/springboot/picpay/simplified/controller/UsuarioController.java
@@ -3,6 +3,10 @@ package com.springboot.picpay.simplified.controller;
 import com.springboot.picpay.simplified.dto.request.UsuarioRequestDTO;
 import com.springboot.picpay.simplified.dto.response.UsuarioResponseDTO;
 import com.springboot.picpay.simplified.service.UsuarioService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -12,21 +16,38 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/usuarios")
 @RequiredArgsConstructor
+@Tag(name = "Usuários", description = "Gerenciamento de usuários")
 public class UsuarioController {
 
     private final UsuarioService usuarioService;
 
+    @Operation(summary = "Cria um novo usuário", description = "Cria um novo usuário")
+    @ApiResponses({
+            @ApiResponse(responseCode = "201", description = "Usuário criado com sucesso"),
+            @ApiResponse(responseCode = "400", description = "Informações de usuário inválidas"),
+            @ApiResponse(responseCode = "409", description = "Informações de usuário conflitantes com outros usuários"),
+    })
     @PostMapping("/criar")
     public ResponseEntity<UsuarioResponseDTO> criarUsuario(@RequestBody @Valid UsuarioRequestDTO dto) {
         return ResponseEntity.status(HttpStatus.CREATED).body(usuarioService.criarUsuario(dto));
     }
 
+    @Operation(summary = "Deleta um usuário por ID", description = "Busca e tenta deletar um usuário por ID")
+    @ApiResponses({
+            @ApiResponse(responseCode = "204", description = "Usuário deletado com sucesso, nada para retornar."),
+            @ApiResponse(responseCode = "404", description = "Usuário não encontrado"),
+    })
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deletarUsuario(@PathVariable Long id) {
         usuarioService.desativarUsuario(id);
         return ResponseEntity.noContent().build();
     }
 
+    @Operation(summary = "Encontra um usuário por ID", description = "Busca e tenta encontrar um usuário por ID")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Usuário encontrado com sucesso"),
+            @ApiResponse(responseCode = "404", description = "Usuário não encontrado"),
+    })
     @GetMapping("/{id}")
     public ResponseEntity<UsuarioResponseDTO> buscarUsuarioPorId(@PathVariable Long id) {
         return ResponseEntity.ok(usuarioService.findUsuarioResponseById(id));
